97aded776ee63c34bf6c826a5dcb9027387efe4f
安田洋介 <double.y.919.quick@gmail.com>
2015-04-20 13:49:38 +0900

    cube light

--- tutorial/main.cpp
+++ tutorial/main.cpp
@@ -1,40 +1,131 @@
 #include <GLUT/glut.h>
 #include <stdlib.h>
-#include <stdio.h>
 
-#define MAXPOINTS 100      /* 記憶する点の数　　 */
-GLint point[MAXPOINTS][2]; /* 座標を記憶する配列 */
-int pointnum = 0;          /* 記憶した座標の数　 */
-int rubberband = 0;        /* ラバーバンドの消去 */
+GLdouble vertex[][3] = {
+    { 0.0, 0.0, 0.0 }, /* A */
+    { 1.0, 0.0, 0.0 }, /* B */
+    { 1.0, 1.0, 0.0 }, /* C */
+    { 0.0, 1.0, 0.0 }, /* D */
+    { 0.0, 0.0, 1.0 }, /* E */
+    { 1.0, 0.0, 1.0 }, /* F */
+    { 1.0, 1.0, 1.0 }, /* G */
+    { 0.0, 1.0, 1.0 }  /* H */
+};
+
+int edge[][2] = {
+    { 0, 1 }, /* ア (A-B) */
+    { 1, 2 }, /* イ (B-C) */
+    { 2, 3 }, /* ウ (C-D) */
+    { 3, 0 }, /* エ (D-A) */
+    { 4, 5 }, /* オ (E-F) */
+    { 5, 6 }, /* カ (F-G) */
+    { 6, 7 }, /* キ (G-H) */
+    { 7, 4 }, /* ク (H-E) */
+    { 0, 4 }, /* ケ (A-E) */
+    { 1, 5 }, /* コ (B-F) */
+    { 2, 6 }, /* サ (C-G) */
+    { 3, 7 }  /* シ (D-H) */
+};
+
+int face[][4] = {
+    { 0, 1, 2, 3 },
+    { 1, 5, 6, 2 },
+    { 5, 4, 7, 6 },
+    { 4, 0, 3, 7 },
+    { 4, 5, 1, 0 },
+    { 3, 2, 6, 7 }
+};
+
+GLdouble color[][3] = {
+    { 1.0, 0.0, 0.0 }, /* 赤 */
+    { 0.0, 1.0, 0.0 }, /* 緑 */
+    { 0.0, 0.0, 1.0 }, /* 青 */
+    { 1.0, 1.0, 0.0 }, /* 黄 */
+    { 1.0, 0.0, 1.0 }, /* マゼンタ */
+    { 0.0, 1.0, 1.0 }  /* シアン 　*/
+};
+
+GLdouble normal[][3] = {
+    { 0.0, 0.0,-1.0 },
+    { 1.0, 0.0, 0.0 },
+    { 0.0, 0.0, 1.0 },
+    {-1.0, 0.0, 0.0 },
+    { 0.0,-1.0, 0.0 },
+    { 0.0, 1.0, 0.0 }
+};
+
+void idle(void)
+{
+    glutPostRedisplay();
+}
 
 void display(void)
 {
     int i;
+    int j;
+    static int r = 0; /* 回転角 */
     
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
-    /* 記録したデータで線を描く */
-    if (pointnum > 1) {
-        glColor3d(0.0, 0.0, 0.0);
-        glBegin(GL_LINES);
-        for (i = 0; i < pointnum; ++i) {
-            glVertex2iv(point[i]);
-        }
-        glEnd();
-    }
-    
-    glFlush();
-}
-void resize(int w, int h)
-{
-    /* ウィンドウ全体をビューポートにする */
-    glViewport(0, 0, w, h);
-    
-    /* 変換行列の初期化 */
     glLoadIdentity();
     
-    /* スクリーン上の座標系をマウスの座標系に一致させる */
-    glOrtho(-0.5, (GLdouble)w - 0.5, (GLdouble)h - 0.5, -0.5, -1.0, 1.0);
+    /* 視点位置と視線方向 */
+    gluLookAt(3.0, 4.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    
+    /* 図形の回転 */
+    glRotated((double)r, 0.0, 1.0, 0.0);
+    
+    /* 図形の描画 */
+    glBegin(GL_QUADS);
+    for (j = 0; j < 6; ++j) {
+        glNormal3dv(normal[j]);
+        for (i = 0; i < 4; ++i) {
+            glVertex3dv(vertex[face[j][i]]);
+        }
+    }
+    glEnd();
+    
+    glutSwapBuffers();
+    
+    /* 一周回ったら回転角を 0 に戻す */
+    if (++r >= 360) r = 0;
+}
+
+void resize(int w, int h)
+{
+    glViewport(0, 0, w, h);
+    
+    /* 透視変換行列の設定 */
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(30.0, (double)w / (double)h, 1.0, 100.0);
+    
+    /* モデルビュー変換行列の設定 */
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void mouse(int button, int state, int x, int y)
+{
+    switch (button) {
+        case GLUT_LEFT_BUTTON:
+            if (state == GLUT_DOWN) {
+                /* アニメーション開始 */
+                glutIdleFunc(idle);
+            }
+            else {
+                /* アニメーション停止 */
+                glutIdleFunc(0);
+            }
+            break;
+        case GLUT_RIGHT_BUTTON:
+            if (state == GLUT_DOWN) {
+                /* コマ送り (1ステップだけ進める) */
+                glutPostRedisplay();
+            }
+            break;
+        default:
+            break;
+    }
 }
 
 void keyboard(unsigned char key, int x, int y)
@@ -49,87 +140,27 @@ void keyboard(unsigned char key, int x, int y)
     }
 }
 
-void mouse(int button, int state, int x, int y)
-{
-    switch (button) {
-        case GLUT_LEFT_BUTTON:
-            /* ボタンを操作した位置を記録する */
-            point[pointnum][0] = x;
-            point[pointnum][1] = y;
-            if (state == GLUT_UP) {
-                /* ボタンを押した位置から離した位置まで線を引く */
-                glColor3d(0.0, 0.0, 0.0);
-                glBegin(GL_LINES);
-                glVertex2iv(point[pointnum - 1]); /* 一つ前は押した位置　 */
-                glVertex2iv(point[pointnum]);     /* 今の位置は離した位置 */
-                glEnd();
-                glFlush();
-                
-                /* 始点ではラバーバンドを描いていないので消さない */
-                rubberband = 0;
-            }
-            else {
-            }
-            if (pointnum < MAXPOINTS) ++pointnum;
-            break;
-        case GLUT_MIDDLE_BUTTON:
-            break;
-        case GLUT_RIGHT_BUTTON:
-            break;
-        default:
-            break;
-    }
-}
-
-void motion(int x, int y)
-{
-    static GLint savepoint[2]; /* 以前のラバーバンドの端点 */
-    
-    /* 論理演算機能 ON */
-    glEnable(GL_COLOR_LOGIC_OP);
-    glLogicOp(GL_INVERT);
-    
-    glBegin(GL_LINES);
-    if (rubberband) {
-        /* 以前のラバーバンドを消す */
-        glVertex2iv(point[pointnum - 1]);
-        glVertex2iv(savepoint);
-    }
-    /* 新しいラバーバンドを描く */
-    glVertex2iv(point[pointnum - 1]);
-    glVertex2i(x, y);
-    glEnd();
-    
-    glFlush();
-    
-    /* 論理演算機能 OFF */
-    glLogicOp(GL_COPY);
-    glDisable(GL_COLOR_LOGIC_OP);
-    
-    /* 今描いたラバーバンドの端点を保存 */
-    savepoint[0] = x;
-    savepoint[1] = y;
-    
-    /* 今描いたラバーバンドは次のタイミングで消す */
-    rubberband = 1;
-}
-
 void init(void)
 {
     glClearColor(1.0, 1.0, 1.0, 1.0);
+    
+    glEnable(GL_DEPTH_TEST);
+    
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_FRONT);
+    
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
 }
 
 int main(int argc, char *argv[])
 {
-    glutInitWindowPosition(10, 10);
-    glutInitWindowSize(320, 240);
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_RGBA);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
     glutCreateWindow(argv[0]);
     glutDisplayFunc(display);
     glutReshapeFunc(resize);
     glutMouseFunc(mouse);
-    glutMotionFunc(motion);
     glutKeyboardFunc(keyboard);
     init();
     glutMainLoop();
